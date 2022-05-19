3c1613881b4c56726a8370ecedb7d8a875a1d83f
Joseph Darville <joseph.darville9@gmail.com>
2022-05-19 00:40:16 -0400

    nearly completed assignment

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Java-Coding-Quiz
+# Password Generator
+
+## Purpose
+A Short quiz on javascript. Score will be based on the remaining time left on the timer.
+Time will be reduced by 10 for each question answered wrong.
+
+## Built With
+* HTML
+* CSS
+* JavaScript
+
+## Website
+https://jdarville9.github.io/Password-Generator/
+
+
+![Website Preview](./assets/images/Screen%20Shot%202022-05-19%20at%2012.28.49%20AM.png)
+![Website Preview](./assets/images/Screen%20Shot%202022-05-19%20at%2012.28.17%20AM.png)
+![Website Preview](./assets/images/Screen%20Shot%202022-05-19%20at%2012.28.05%20AM.png)
+
+## Contribution
+Made with ❤️ by Joseph Darville
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,150 @@
-var questionEl = document.querySelector("#question-prompt");
+// Query Selectors
+var quizEl = document.querySelector("#start-quiz");
+var answerEl = document.querySelector("#answers");
+var submitScoreEl = document.querySelector("#submit-score");
+var initialsEl = document.querySelector("#user-initials");
+var showQuestionBlock = document.querySelector("#inner-block");
+var hideStartMenu = document.querySelector("#quiz-intro");
+var showQuestionBlock = document.querySelector("#inner-block");
+var questionEl = document.querySelector("#question");
+var timerEl = document.querySelector("#timer");
+var buttonClick = document.querySelector("#submit-score");
+var finishQuiz = document.querySelector("#finish-quiz")
+var pointsScored = document.querySelector("#points-scored")
 
+var playerInit;
 
+// TIMER STUFF
+var startingTime = 100;
+var countDown;
 
+var timer = function() {
+
+    countDown = setInterval (()=> {
+        startingTime--;
+
+        if (startingTime >= 0) {
+            timerEl.innerText = startingTime
+        } else {
+            clearInterval(startingTime);
+            highScore();
+        }
+    },1000)
+}
+// END TIMER STUFF
+
+var quizData = [
+    {
+        question: "What is JavaScript?",
+        answers: [
+            "An online streaming service.",
+            "An object-oriented computer programming language commonly used to create interactive effects within web browsers.",
+            "A standardized system for tagging text files to achieve font, color, graphic, and hyperlink effects on World Wide Web pages.",
+            "A popular coffe shop located in Central Park",],
+        correct: "An object-oriented computer programming language commonly used to create interactive effects within web browsers.",
+
+    },
+    {
+        question: "Which html element is used to link a JavaScript source?",
+        answers: ["<link>", ".js", "src ''", "<script>",],
+        correct: "<script>",
+    },
+    {
+        question: "Which document method will return the first element within the document that matches a specified selector, or group of selectors?",
+        answers: [".window.alert()", ".querySelector()", ".addEventListener()", ".createElement()",],
+        correct: ".querySelector()",
+    },
+    {
+        question: "Which of these is not a recoginzed value in JS",
+        answers: ["String", "Number", "Boolean", "Quote",],
+        correct: "Quote",
+    },
+];
+
+function start() {
+    timer();
+    currentQuestion = 0;
+    hideStartMenu.classList.add("hide");
+    showQuestionBlock.classList.remove("hide");
+    loadQuestions();
+}
+
+var loadQuestions = function() {
+    questionEl.textContent = quizData[currentQuestion].question;
+    var answers = quizData[currentQuestion].answers;
+    answerEl.innerHTML = "";
+
+    for(var i = 0; i < answers.length; i++) {
+        const option = answers[i];
+        var answerOption = document.createElement("button");
+        answerOption.textContent = option;
+        answerOption.classList.add("question-option");
+        answerOption.addEventListener("click", check)
+        answerEl.appendChild(answerOption)
+    }
+}
+
+var check = function(event) {
+    var answerChoice = event.target;
+    var correctAnswer = answerChoice.innerText === quizData[currentQuestion].correct;
+
+    if (!correctAnswer) {
+        startingTime = startingTime - 10;
+        alert("Awe, that was a lame answer. 10 seconds off Time Left")
+    } else {
+        alert("You Rock!!");
+    }
+
+    currentQuestion++;
+
+    if (currentQuestion < quizData.length) {
+        loadQuestions();
+    } 
+    else {
+        submitScore();
+    }
+}
+
+var submitScore = function() {
+    clearInterval(countDown);
+    timerEl.innerText = "000"
+
+    showQuestionBlock.classList.add("hide");
+    finishQuiz.classList.remove("hide");
+    pointsScored.innerText = startingTime;
+}
+
+var viewScores = function() {
+    finishQuiz.classList.add("hide");
+
+    var saveScores = function() {
+        localStorage.setItem("storeHighScore", JSON.stringify(storeHighScore))
+    }
+
+    var loadScores = function() {
+    }
+
+    var enterInit = document.querySelector("#enter-initials").value
+    if (enterInit.length === 3) {
+        var scoreLi = document.querySelector("#player-info")
+        var listItemEl = document.createElement("p")
+        listItemEl.innerText = enterInit + " scored " + pointsScored.innerText + " points!";
+        var storeHighScore = listItemEl.innerText
+        scoreLi.appendChild(listItemEl)
+        console.log("Checking High Scores")
+
+        var scorePage = document.querySelector("#high-scores")
+        scorePage.classList.remove("hide");
+
+        saveScores();
+
+    } else {
+        alert("The quiz Gods DEMAND that you enter only 3 characters. No more, no less!")
+        document.querySelector("#enter-initials").value = ""
+        submitScore()
+    }
+}
+
+// Event Listeners
+buttonClick.addEventListener("click", viewScores);
+quizEl.addEventListener("click", start);
